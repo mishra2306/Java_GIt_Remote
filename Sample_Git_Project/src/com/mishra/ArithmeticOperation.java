c620e3570661200b90225e442ccc59a918847a16
mishra2306 <n.mishra2306@gmail.com>
2017-04-30 17:44:17 +0530

    TL Added New Resource to Master

---
+++ Sample_Git_Project/src/com/mishra/ArithmeticOperation.java
@@ -0,0 +1,9 @@
+package com.mishra;
+
+public class ArithmeticOperation {
+	
+	public int operataion(int a, int b)
+	{
+		return a+b;
+	}
+}
--- Sample_Git_Project/src/com/nagendra/Sample.java
+++ Sample_Git_Project/src/com/nagendra/Sample.java
@@ -1,13 +1,18 @@
 package com.nagendra;
 
+import com.mishra.ArithmeticOperation;
+
 public class Sample {
 
 	public static void main(String[] args) {
+		ArithmeticOperation operation = null;
 		System.out.println("Hello Git 1st Program");
 		System.out.println("Update from Nitin");
-
 		System.out.println("Bas Kar Le Bhai");
 		
+		// Adding New Functionality
+		operation = new ArithmeticOperation();
+		System.out.println("Add Operation : "+operation.operataion(200, 300));
+		
 	}
-
 }
