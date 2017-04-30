bc78172d756772ea8fbf436b3cd1d56a9b35e535
nitinpawar283 <nitinpawar283@gmail.com>
2017-04-30 17:47:54 +0530

    Commit 3

--- Sample_Git_Project/src/com/nagendra/Sample.java
+++ Sample_Git_Project/src/com/nagendra/Sample.java
@@ -3,11 +3,11 @@ package com.nagendra;
 public class Sample {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello Git 1st Program");
 		System.out.println("Update from Nitin");
-
 		System.out.println("Bas Kar Le Bhai");
-		
+
 	}
 
 }
